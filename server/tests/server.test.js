4ab1282138c4dae20466ec37515cd5f7b396b3f7
Indranil Chatterjee <indranil.chatterjee@pb.com>
2018-11-05 17:40:06 +0530

    Added tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -230,7 +230,7 @@ describe('POST /users', () => {
                     expect(user).toBeTruthy();
                     expect(user.password).not.toBe(password);
                     done();
-                })
+                }).catch(e => done(e))
             });
     });
 
@@ -248,5 +248,56 @@ describe('POST /users', () => {
         .send({email: users[0].email, password: '$Unanda26'})
         .expect(400)
         .end(done);
+    });
+});
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', done => {
+        var email = users[1].email;
+        var password = users[1].password;
+
+        request(app)
+            .post('/users/login')
+            .send({email, password})
+            .expect(200)
+            .expect(res => {
+                expect(res.header['x-auth']).toBeTruthy();
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then(user => {
+                    expect(user.tokens[0]).toMatchObject({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch(e => done(e));
+            });
+    });
+
+    it('should reject invalid login', done => {
+        var email = users[1].email;
+        var password = users[1].password + '1';
+
+        request(app)
+            .post('/users/login')
+            .send({email, password})
+            .expect(400)
+            .expect(res => {
+                expect(res.header['x-auth']).toBeFalsy();
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then(user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(e => done(e));
+            });
     });
 });
